e4af9886061b055915f6a8dc03deae9ef24a4d9d
onkar lad <onkarlad426@gmail.com>
2025-01-02 16:20:14 +0530

    redirected towards whatsapp with message

--- script.js
+++ script.js
@@ -10,6 +10,48 @@ menu.onclick = () =>{
 }
 
 
+const first_order = document.getElementById('one_dozen');
+
+first_order.addEventListener("click", function(event){
+
+    event.preventDefault();
+    const message = encodeURIComponent('I want 1 Dozen mangoes');
+    const dynamicPath1 = `https://wa.me/9892900645?text=${message}`; 
+    first_order.href = dynamicPath1;
+
+    //console.log(dynamicPath1);
+    window.open(dynamicPath1, '_blank');
+
+})
+
+
+const second_order = document.getElementById('three_dozen');
+
+second_order.addEventListener("click", function(event){
+
+    event.preventDefault();
+    const message = encodeURIComponent('I want 3 Dozen mangoes');
+    const dynamicPath2 = `https://wa.me/9892900645?text=${message}`; 
+    second_order.href = dynamicPath2;
+
+    //console.log(dynamicPath1);
+    window.open(dynamicPath2, '_blank');
+
+})
+
+const third_order = document.getElementById('five_dozen');
+
+third_order.addEventListener("click", function(event){
+
+    event.preventDefault();
+    const message = encodeURIComponent('I want 5 Dozen mangoes');
+    const dynamicPath3 = `https://wa.me/9892900645?text=${message}`; 
+    third_order.href = dynamicPath3;
+
+    //console.log(dynamicPath1);
+    window.open(dynamicPath3, '_blank');
+
+})
 
 
 
